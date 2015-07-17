52991ef4a3124316460e970dc45307924d4e2f5f
olgerjuancarlosvictor <olgerccc@gmail.com>
2015-07-16 23:46:38 -0500

    CREANDO MI MOLDELO AUTOR

--- BibliotecaVirtual/apps/autores/models.py
+++ BibliotecaVirtual/apps/autores/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
 class Autor(models.Model):
 	nombre = models.CharField(max_length=50)
+	apellido = models.CharField(max_length=50)
 	pais = models.CharField(max_length=50)
 	descripcion = models.TextField(max_length=200)
 	#foto = models.ImageField(upload_to='foto_autor')
+# Create your models here.
