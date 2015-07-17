caaab213bfe71be599af15861cb404e6fab44a6b
olgerjuancarlosvictor <olgerccc@gmail.com>
2015-07-16 21:40:25 -0500

    mi vista basada en clases

--- BibliotecaVirtual/apps/inicio/urls.py
+++ BibliotecaVirtual/apps/inicio/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, url
 
+from apps.inicio.views import index2
 
 urlpatterns = patterns('apps.inicio.views',
 	                   url(r'^$','index_view', name="index"),
-	                   #url(r'^index/$',index2.as_view(), name="index2"),
-
+	                   
+	                   url(r'^index/', index2.as_view()),
+                    
 	                  )
+
+
