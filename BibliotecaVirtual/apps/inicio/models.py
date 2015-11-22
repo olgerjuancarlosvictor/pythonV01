dae1101df73732ae7e38bf813ae4b557531e259f
olgerjuancarlosvictor <olgerccc@gmail.com>
2015-11-21 20:50:48 -0500

    AVANCE PYTHON3 2015_II

--- BibliotecaVirtual/apps/inicio/models.py
+++ BibliotecaVirtual/apps/inicio/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Perfiles(models.Model):
+	usuario = models.OneToOneField(User)
+	#telefono = models.IntegerField()
+	num_tele = models.IntegerField()
+
+	def __unicode__(self):
+		return self.usuario.username
--- BibliotecaVirtual/apps/inicio/urls.py
+++ BibliotecaVirtual/apps/inicio/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, url
-
+from .views import Registrarse
 
 urlpatterns = patterns('',
 	                   
                     url(r'^$' , 'django.contrib.auth.views.login',
                     	{'template_name': 'inicio/index.html'} , name='login'),
                     url(r'^cerrar/$' , 'django.contrib.auth.views.logout_then_login',
-                    	 name='logout')
-	                  )
+                    	 name='logout'),
+                    url(r'^registrarse/$' , Registrarse.as_view() , name='registrarse'),
+                )
 
 
